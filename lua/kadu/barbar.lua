177bfb72b7e6cc268b962103255afbc101279cf4
kaduhod <carlosjr.ribas@gmail.com.br>
2023-10-21 00:15:07 -0300

    desablitando icones

--- lua/kadu/barbar.lua
+++ lua/kadu/barbar.lua
@@ -3,15 +3,15 @@ local barbar = require('barbar')
 --
 -- Configuração do Barbar.nvim
 barbar.setup {
-  -- Opção para fechar o Neovim quando o último buffer é fechado
-  close_on_last_tab = true,
+	-- Opção para fechar o Neovim quando o último buffer é fechado
+	close_on_last_tab = true,
+	icons = { filetype = { enabled = false } },
+	-- Mostrar números nas guias
+	show_numbers = true,
+	-- Configuração de realce das guias
+	show_tabpages = true,
+	show_filename_only = true,
 
-  -- Mostrar números nas guias
-  show_numbers = true,
-
-  -- Configuração de realce das guias
-  show_tabpages = true,
-  show_filename_only = true,
 }
 
 -- Configurar os mapeamentos (teclas de atalho) para navegar entre as guias
