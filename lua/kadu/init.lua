49897e2908ebf6005d66075bf20b4601349c8c0e
kaduhod <carlosjr.ribas@gmail.com.br>
2024-07-21 12:32:11 -0300

    :sparkles: Adicionado tamplete tmpl para snippets de html

--- lua/kadu/init.lua
+++ lua/kadu/init.lua
@@ -6,6 +6,7 @@ require("kadu.completions")
 vim.api.nvim_exec([[
   augroup FiletypeDetect
     au BufNewFile,BufRead *.hbs setfiletype html
+    au BufNewFile,BufRead *.tmpl setfiletype html
   augroup END
 ]], false)
 print('Arquivo kaduhod carregado!')
