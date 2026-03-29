ca6b46cec736003a7aa175b243415b814e631251
Carlos Ribas :: MacOs <carlosjr.ribas@gmail.com>
2026-03-29 14:15:34 -0300

    Adicionando comandos para o git

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gf', '<cmd>Gdiffsplit<cr>')
+vim.keymap.set('n', '<leader>gh', ':diffget<CR>]c', { desc = 'Fugitive: Descartar hunk (pull do index)' })
+vim.keymap.set('n', '<leader>ga', ':diffput<CR>]c', { desc = 'Git: Aceitar hunk e pular' })
