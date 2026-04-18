e73e7f81a6d672b473f9074dc4f0b02602e0acff
kaduhod <carlosjr.ribas@gmail.com.br>
2026-04-18 10:48:29 -0300

    Corrigido config de snippet e adicionado snippet de ejs block

--- lua/kadu/luasnip.lua
+++ lua/kadu/luasnip.lua
@@ -1,19 +1,13 @@
 local ls = require 'luasnip'
-ls.filetype_extend("html", { "tmpl", "ejs" })
+ls.filetype_extend("ejs", { "html" })
+ls.filetype_extend("tmpl", { "html" })
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
-
-ls.snippets = {
-  lua = {
-    s("doc", {
-      t({ "/**", " * " }),
-      i(1, "Descrição da função"),
-      t({ "", " * ", " * @param " }),
-      i(2, "param"),
-      t({ "", " * @return " }),
-      i(3, "return"),
-      t({ "", " */" }),
-    }),
-  },
-}
+ls.add_snippets("html", {
+  s("pr", {
+    t("<%= "),
+    i(1),
+    t(" %>"),
+  }),
+})
